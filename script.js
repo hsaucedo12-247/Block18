8022ac95dc8800b14cdb67bdb60b02de678dd341
Humberto Saucedo <hsaucedo6@gmail.com>
2024-10-28 19:17:58 -0600

    secondtry

---
+++ script.js
@@ -0,0 +1,29 @@
+/*
+
+* function multiplication
+* returns the product of 2 imput numbers
+* Expect Multiplication (2,4) to be a number.
+* Expect Multiplication (3,4) to be equal to 12
+* Expect Multiplication ("b",4) to be an error
+
+#2
+
+*function concat odds
+*returns odd numbers in new array in ascending order
+*Expect concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) to be [-1, 1, 3, 9, 15]
+
+#3
+
+* Cart checkout functional test
+* if member logs in they get points and 
+* if they dont their store points dont save
+
+* if the shopping cart is empty all checlout buttons are disabled
+* ex. if cart is empty, continue to payment button is dissabled
+* ex. if cart is empty then screen will prompt to scan item
+
+* when going through checkout member should see different prompts
+* member will see login or checkout as guest screen
+* member will see list of items in cart
+* member will be prompted to continue to payment method
+
